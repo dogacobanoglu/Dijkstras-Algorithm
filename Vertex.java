cb2e9009022097ba1e4570c285abf7f89f7e2a91
Doğa Çobanoğlu <93142130+dogacobanoglu@users.noreply.github.com>
2022-07-30 22:17:46 +0300

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,9 @@
+public class Vertex {
+    public int lastVertexInPath;
+    public int weightOfPath;
+
+    public Vertex(int lastVertexInPath, int weightOfPath){
+        this.lastVertexInPath = lastVertexInPath;
+        this.weightOfPath = weightOfPath;
+    }
+}
